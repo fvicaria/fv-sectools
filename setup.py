6a4c87bd9ed19a240fcc9e2ee97268e30276d7e8
fvicaria <4Driver$>
2021-07-30 18:24:39 +0100

    Still cleaning up.

--- setup.py
+++ setup.py
@@ -3,11 +3,11 @@ from distutils.core import setup
 
 setup(name='fv-sectools',
       description='A set of IP-based security checks for websites and applications',
-      long_description=open('README.md').read(),
+      long_description=open('README.rst').read(),
       version='0.1dev',
       author='F Vicaria',
       author_email='fvicaria@hotmail.com',
-      url='https://www.vicaria.org',
+      url='http://www.vicaria.org/',
       packages=['fv-sectools', ],
 	  python_requires='>=3.6',
       license='MIT License',
